a721667fbad7e0c3266e7b669b2a2b61b7e0b241
Gaoooooooo24 <k20078062@kcl.ac.uk>
2021-10-30 09:30:20 -0700

    update

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import SignUpForm
+'''from .models import User'''
+from django.contrib.auth import authenticate,login, logout
+
+def feed(request):
+    return render(request, 'feed.html')
+
+'''
+def log_in(request):
+     if request.method == 'POST':
+         form = LogInForm(request.POST)
+         if form.is_valid():
+             username = form.cleaned_data.get('username')
+             password = form.cleaned_data.get('password')
+             user = authenticate(username=username, password=password)
+             if user is not None:
+                 login(request, user)
+                 return redirect('feed')
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
+    '''
 
 def home(request):
     return render(request, 'home.html')
 
 def sign_up(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+    else:
+        form = SignUpForm()
     return render(request, 'sign_up.html', {'form': form})
 
 # Create your views here.
