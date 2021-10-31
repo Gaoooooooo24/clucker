36f047899a23e7cd1afb453d9a0c926e6ea145c6
Gaoooooooo24 <k20078062@kcl.ac.uk>
2021-10-30 18:42:14 -0700

    Create log_in and log_out

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,8 +1,29 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate,login, logout
 from django.shortcuts import redirect, render
-from .forms import SignUpForm
+from .forms import LogInForm, SignUpForm
 
 def feed(request):
-    return render(request, 'feed.html')
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
+
+def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     return render(request, 'home.html')
@@ -11,7 +32,8 @@ def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('feed')
     else:
         form = SignUpForm()
